f60bb284aa71e34d8257e300a979d3692558b691
aaayounis94 <62307178+aaayounis94@users.noreply.github.com>
2021-10-26 15:53:30 +0200

    main commet
    main commet

---
+++ src/components/AppChild.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="item">
+    <h2>{{ item }}</h2>
+    <img :src="url" :alt="item" width="235" height="350" />
+    <div class="quantity">
+      <button class="dec" @click="counter > 0 ? counter-- : 0"></button>
+      <span class="quant-text">Quantity: {{ counter }}</span>
+      <button class="inc" @click="counter++"></button>
+    </div>
+    <button class="submit">submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item", "url"],
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+};
+</script>
